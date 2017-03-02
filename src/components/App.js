e5ed814e102c4c85582f568f44aa8d45accc9024
Cody Loyd <codyloyd@gmail.com>
2017-03-01 20:58:28 -0600

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,14 @@ class App extends Component {
             detentions={this.state.detentions}
           />
         </div>
+        <footer className="footer">
+          <div className="container">
+            <div className="content has-text-centered">
+              <strong>Detention Tracker</strong> by <a href="http://codyloyd.com">Cody Loyd</a>. The source code is licensed 
+        <a href="http://opensource.org/licenses/mit-license.php"> MIT</a> and can be found <a href="https://github.com/codyloyd/detention-tracker">HERE</a>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
