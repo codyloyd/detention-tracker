acda17c3b10d81c51519d2fb50a5b308fcbbc85a
Cody Loyd <codyloyd@gmail.com>
2017-03-16 08:09:33 -0500

    add verification and error message to sign-in page

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,25 @@
 import firebase from 'firebase'
 
 export const signIn = ({email, password}) => {
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(error => {
-    const errorCode = error.code
-    const errorMessage = error.message
-    console.log({errorCode, errorMessage})
-  })
-}
+  return firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(error => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      return Promise.reject({errorCode, errorMessage})
+    })
+};
 
 export const signOut = () => {
   firebase.auth().signOut().then(
     function () {
       console.log('signedout')
     },
-    function (error) {}
+    function (error) {
+      console.log(error)
+    }
   )
-}
+};
 
 export const currentUser = () => firebase.auth().currentUser
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,44 +1,70 @@
-import React from 'react'
-import * as auth from '../auth'
+import React from 'react';
+import * as auth from '../auth';
 
-const SignIn = props => {
-  let email, password
-  return (
-    <div>
-      <div className="column is-5">
-        <form
-          onSubmit={e => {
-            e.preventDefault()
-            auth.signIn({email: email.value, password: password.value})
-          }}
-        >
-          <label className="label">email:</label>
-          <div className="control">
-            <input
-              type="email"
-              className="input"
-              name="email"
-              placeholder="your school email address"
-              ref={input => email = input}
-            />
-          </div>
-          <label className="label">password:</label>
-          <div className="control">
-            <input
-              type="password"
-              className="input"
-              name="password"
-              placeholder="password"
-              ref={input => password = input}
-            />
-          </div>
-          <div className="control">
-            <input type="submit" className="button" value="Sign In" />
-          </div>
-        </form>
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      name: '',
+      error: null
+    };
+  }
+  handleInputChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+  render() {
+    return (
+      <div>
+        <div className="column is-5">
+          {this.state.error
+            ? <div className="notification is-danger">
+                OOPS! Something went wrong: <br />
+                {this.state.error}
+              </div>
+            : null}
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              auth
+                .signIn({
+                  email: this.state.email,
+                  password: this.state.password
+                })
+                .catch(error => {
+                  console.log(error);
+                  this.setState({error: error.errorMessage});
+                });
+            }}
+          >
+            <label className="label">email:</label>
+            <div className="control">
+              <input
+                type="email"
+                className="input"
+                name="email"
+                placeholder="your school email address"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <label className="label">password:</label>
+            <div className="control">
+              <input
+                type="password"
+                className="input"
+                name="password"
+                placeholder="password"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="control">
+              <input type="submit" className="button" value="Sign In" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default SignIn
+export default SignIn;
