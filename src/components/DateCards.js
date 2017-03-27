c18dfcfe8c3d89012760a7f2e0dcd5f120aece30
Cody Loyd <codyloyd@gmail.com>
2017-03-27 10:38:54 -0500

    add no detentions message

--- src/components/DateCards.js
+++ src/components/DateCards.js
@@ -2,13 +2,17 @@ import React from 'react'
 import DateCard from './DateCard'
 
 const DateCards = ({detentions}) => {
-  return (
-    <div>
-      {Object.keys(detentions).sort((a, b) => a > b).map(d => {
-        return <DateCard key={d} detentions={detentions[d]} date={d} />
-      })}
-    </div>
-  )
-}
+  return Object.keys(detentions).length
+    ? <div>
+        {Object.keys(detentions).sort((a, b) => a > b).map(d => {
+          return <DateCard key={d} detentions={detentions[d]} date={d} />
+        })}
+      </div>
+    : <div className="section is-medium">
+        <p className="title has-text-centered">
+          there are no upcoming detentions
+        </p>
+      </div>
+};
 
 export default DateCards
