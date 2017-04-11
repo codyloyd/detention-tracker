132ad51a64972dd9d1db7b66813da694ded00390
Cody Loyd <codyloyd@gmail.com>
2017-04-11 10:11:44 -0500

    animate modal

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -3,32 +3,38 @@ import {confirmable} from 'react-confirm'
 
 const ConfirmationDialog = confirmable(props => {
   console.log(props)
-  return (
-    <div className={`modal ${props.show ? 'is-active' : null}`}>
-      <div className="modal-background" />
-      <div className="modal-content">
-        <div className="box">
-          <div className="p">Are you sure you want to delete that?</div>
-          <div className="control is-grouped">
-            <p className="control">
-              <a
-                className="button is-primary"
-                onClick={() => props.cancel('cancelled')}
-              >
-                NO!
-              </a>
-            </p>
-            <p className="control">
-              <a className="button is-danger" onClick={() => props.proceed()}>
-                DELETE IT!
-              </a>
-            </p>
+  return props.show
+    ? <div className={`modal is-active`}>
+        <div
+          className="modal-background"
+          onClick={() => props.cancel('cancelled')}
+        />
+        <div className="modal-content">
+          <div className="box">
+            <div className="p">Are you sure you want to delete that?</div>
+            <div className="control is-grouped">
+              <p className="control">
+                <a
+                  className="button is-primary"
+                  onClick={() => props.cancel('cancelled')}
+                >
+                  NO!
+                </a>
+              </p>
+              <p className="control">
+                <a className="button is-danger" onClick={() => props.proceed()}>
+                  DELETE IT!
+                </a>
+              </p>
+            </div>
           </div>
         </div>
+        <div
+          className="modal-close"
+          onClick={() => props.cancel('cancelled')}
+        />
       </div>
-      <div className="modal-close" />
-    </div>
-  )
+    : null
 })
 
 export default ConfirmationDialog
